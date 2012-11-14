c3b639445dc7e9e98b26dc31bc0548c9c6fbd000
Matthew <matt875@gmail.com>
2012-11-13 22:10:33 -0500

    Pretty much balls

--- js/main.js
+++ js/main.js
@@ -22,9 +22,11 @@ $(function () {
 	var COLOR_MOVEMENT_HOVER = '#E2C05A';
 	var COLOR_OPPONENT = '#FF007A';
 
-	var TEAM_SIZE = 6;
+	var TEAM_SIZE = 5;
 	
 	var my_team = [];
+	var football = null;
+
 	var game = {
 		turn: 1,
 		stage: 1,
@@ -41,6 +43,7 @@ $(function () {
 		this.level = 1;
 		this.location = 0;
 		this.next_move = -1;
+		this.hasBall = 0;
 		this.move = function (target) {
 			$($('.field_square')[this.location]).html('');
 			this.location = target;
@@ -122,9 +125,98 @@ $(function () {
 		}
 	}
 
+	function Football() {
+		this.id = 0;
+		this.location = -1;
+		this.next_move = -1;
+		this.move = function (target) {
+			$($('.field_square')[this.location]).html('');
+			this.location = target;
+			placeBall(this.location);
+		};
+
+		this.create_move = function (target) {
+			resetMovementField();
+			this.next_move = target;
+			var ball_pos = $($('.field_square')[this.location]).offset();
+			var next_pos = $($('.field_square')[target]).offset();
+
+			var w = next_pos.left - ball_pos.left;
+			var h = next_pos.top - ball_pos.top;
+
+			if(w > 0) {
+				if(h == 0) {
+					h = 1;
+				} else {
+					w = 1;
+				}
+				var start_pos = ball_pos;
+			} else if(w < 0) {
+				w *= -1;
+				var start_pos = next_pos;
+			} else if(h > 0) {
+				var start_pos = ball_pos;
+				w = 1;
+			} else if(h < 0) {
+				var start_pos = next_pos;
+				h *= -1;
+				w = 1;
+			}
+
+			var t = start_pos.top+(PLAYER_SIZE);
+			var l = start_pos.left+(PLAYER_SIZE);
+
+			$('#field').append('<div class="next_move_marker" style="position:absolute;top:'+t+'px;left:'+l+'px;width:'+w+'px;height:'+h+'px;border: 1px dashed;"></div>');
+
+			$($('.field_square')[this.location]).find('div').unbind('click');
+			$($('.field_square')[this.location]).find('div').css('background-color', COLOR_PLAYER_MOVED);
+			renderHUD();
+		};
+
+		this.showMovement = function (player) {
+			resetMovementField();
+			var current_row = Math.floor((player.location/FIELD_WIDTH)+1);
+
+			// If another player is in the way, stop
+			// forward
+			for(var i=1; i<=player.stats.skill;i++) {
+				if(!showSquare(player.location+i, current_row))
+					break;
+			}
+
+			// backward
+			for(var i=1; i<=player.stats.skill;i++) {
+				if(!showSquare(player.location-i, current_row))
+					break;
+			}
+
+			// up
+			for(var i=1; i<=player.stats.skill;i++) {
+				if(!showSquare(player.location-(FIELD_WIDTH*i), -1))
+					break;
+			}
+
+			// down
+			for(var i=1; i<=player.stats.skill;i++) {
+				if(!showSquare(player.location+(FIELD_WIDTH*i), -1))
+					break;
+			}
+
+			$('.move_to').click(function(){
+				football.create_move($(".field_square").index(this));
+			});
+		}
+	}
+
+
 	var GAME_TYPES = [{
 			name: 'offense',
 			positions: [{
+					distance: 2,
+					speed: 1,
+					skill: 2,
+					position: 'center'
+				},{
 					distance: 2,
 					speed: 1,
 					skill: 2,
@@ -139,6 +231,11 @@ $(function () {
 					speed: 2,
 					skill: 2,
 					position: 'receiver'
+				}, {
+					distance: 1,
+					speed: 1,
+					skill: 5,
+					position: 'qb'
 				}
 			]
 		}, {
@@ -153,6 +250,16 @@ $(function () {
 					speed: 2,
 					skill: 1,
 					position: 'safety'
+				}, {
+					distance: 3,
+					speed: 2,
+					skill: 2,
+					position: 'safety'
+				}, {
+					distance: 3,
+					speed: 2,
+					skill: 2,
+					position: 'line back'
 				}, {
 					distance: 3,
 					speed: 2,
@@ -176,15 +283,22 @@ $(function () {
 	}
 	table+='</table>';
 	$('#field').html(table);
-	
+
 	// Create menu
-	var menu = '<div id="menu" style="height: 130px; width: 80px; position:absolute; background-color:#B7B7B7;display:none;"><div>Menu</div><div id="move">move</div></div>';
+	var menu = '<div id="menu" style="height: 130px; width: 80px; position:absolute; background-color:#B7B7B7;display:none;"><div>Menu</div><div id="move">move</div><div id="throw" style="display:none;">throw</div></div>';
 	$('body').append(menu);
 	
 	// Action logic
-	$('#move').click(function(){
+	$('#move').click(function() {
 		var player_index = $('#menu').attr('player');
 		my_team[player_index].showMovement();
+		$('#menu').hide();
+	});
+
+	$('#throw').click(function() {
+		var player_index = $('#menu').attr('player');
+		my_team[player_index].hasBall = 0;
+		football.showMovement(my_team[player_index]);
 		$('#menu').hide();
 	});
 
@@ -217,7 +331,8 @@ $(function () {
 			socket.emit('end_turn', {
 				name: $('#name').val(), 
 				game_state: game,
-				team: my_team
+				team: my_team,
+				ball: football
 			});
 			game.turn_end = true;
 			game_log('game_log', 'Waiting for opponent');
@@ -233,12 +348,15 @@ $(function () {
 			my_team[i].next_move = data.team[i].next_move;
 		};
 		$(".player").remove();
+		$(".field_square").removeClass("ball_here");
 		$(".next_move_marker").remove();
 		$(".opponent_player").remove();
 		
 		renderHUD();
 		renderTeam(my_team);
 		renderTeam(data.other_team);
+		renderBall(data.ball);
+		console.log(data.ball);
 		game.turn_end = false;
 	});
 
@@ -270,7 +388,11 @@ $(function () {
 			var newPlayer = new Player();
 			newPlayer.id = my_team.length;
 			newPlayer.location = generatePlayerPosition();
-			newPlayer.stats = GAME_TYPES[game.type].positions[randomFromInterval(0, GAME_TYPES[game.type].positions.length-1)];
+			newPlayer.stats = GAME_TYPES[game.type].positions[i];
+			if (newPlayer.stats.position === 'qb') {
+				newPlayer.hasBall = 1;
+				football.location = newPlayer.location;
+			}
 			my_team.push(newPlayer);
 		}
 
@@ -301,6 +423,10 @@ $(function () {
 		return false;
 	}
 
+	function renderBall (ball) {
+		placeBall(ball.location);
+	}
+
 	function renderTeam(team) {
 		if(team == my_team)
 			var color = COLOR_PLAYER;
@@ -308,6 +434,11 @@ $(function () {
 			var color = COLOR_OPPONENT;
 		for (var i = 0; i < team.length; i++) {
 			placePlayer(team[i].id, team[i].stats.position, team[i].location, color);
+			if (team[i].hasBall === 1) {
+				football.location = team[i].location;
+				placeBall(team[i].location);
+			}
+
 		};
 		renderHUD();
 		$(".player").hover(
@@ -323,12 +454,19 @@ $(function () {
 		
 		$(".player").click(function(){
 			var pos = $(this).offset();
-			var g = $(this).width()/2;
-			$('#menu').show();
-			$('#menu').offset({top: pos.top+g, left: pos.left+g});
+			var g = $(this).width()/2
 			var player_id = $(this).attr('player_id');
-
+			var player = my_team[player_id];
+			console.log(player);
+			//Create menu based on selected player		
+			if (player.stats.position === 'qb' && player.hasBall === 1)	{
+				$('#throw').show();
+			} else {
+				$('#throw').hide();
+			}		
 			$('#menu').attr('player', player_id);
+			$('#menu').offset({top: pos.top+g, left: pos.left+g});
+			$('#menu').show();
 		});
 	}
 
@@ -340,15 +478,19 @@ $(function () {
 		$($('.field_square')[pos]).html('<div player_id="'+id+'" class="'+player_div_id+'" style="background-color:'+color+';width:'+PLAYER_SIZE+'px;height:'+PLAYER_SIZE+'px;">'+name+'</div>');
 	}
 
+	function placeBall(pos) {
+		console.log(football);
+		console.log(pos);
+		$($('.field_square')[pos]).addClass('ball_here');
+	}
+
 	function showSquare(square, current_row) {
 		var target_row = Math.floor((square/FIELD_WIDTH)+1);
 
 		if(current_row == -1 || current_row == target_row) {
-			if(!isPlayerHere(square)) {
-				$($(".field_square")[square]).css('background-color', COLOR_MOVEMENT);
-				$($(".field_square")[square]).addClass('move_to');
-				return true;
-			}
+			$($(".field_square")[square]).css('background-color', COLOR_MOVEMENT);
+			$($(".field_square")[square]).addClass('move_to');
+			return true;
 		}
 		return false;
 	}
@@ -383,6 +525,9 @@ $(function () {
 	function generateRoom() {
 		var host = true;
 		var single_player = false;
+
+		
+		football = new Football();
 
 		if($('#name').val() == '') {
 			$('#name').val('Dude-'+randomFromInterval(1, 100));
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 9080;
 
 server.listen(port);
 
@@ -50,26 +50,50 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('end_turn', function (data) {
-    if(rooms[socket.room_id].complete) {
+    var other_player = rooms[socket.room_id].complete;
+    if(other_player) {
 
       data.game_state.turn++;
+      other_player.game_state.turn++;
+
+      // Move both teams players
       for (var i = data.team.length - 1; i >= 0; i--) {
         if(data.team[i].next_move != -1)
           data.team[i].location = data.team[i].next_move;
-        data.team[i].next_move = -1;
+      
+        if(other_player.team[i].next_move != -1)
+         other_player.team[i].location = other_player.team[i].next_move;
+       
+        data.team[i].next_move = other_player.team[i].next_move = -1;
       };
 
-      rooms[socket.room_id].complete.game_state.turn++;
-      for (var i = rooms[socket.room_id].complete.team.length - 1; i >= 0; i--) {
-        if(rooms[socket.room_id].complete.team[i].next_move != -1)
-          rooms[socket.room_id].complete.team[i].location = rooms[socket.room_id].complete.team[i].next_move;
-        rooms[socket.room_id].complete.team[i].next_move = -1;
-      };
 
-      data.other_team = rooms[socket.room_id].complete.team;
-      rooms[socket.room_id].complete.other_team = data.team;
+      //Move Da balls
+      if(data.ball.location != -1) {
+        var ball = data.ball;
+      } else {
+        var ball = other_player.ball;
+      }
+
+      console.log(ball);
+      
+      if(ball.next_move != -1) {
+        data.ball.location = other_player.ball.location = ball.next_move;
+        for (var i = data.team.length - 1; i >= 0; i--) {
+          data.team[i].hasBall = other_player.team[i].hasBall =  0;
+          //Ball Catching super simple logic
+          if(data.team[i].location === ball.next_move)
+            data.team[i].hasBall = 1;
+          else if(other_player.team[i].location === ball.next_move)
+            other_player.team[i].hasBall = 1;
+        }
+
+      }
+      console.log(data.ball);
+      data.other_team = other_player.team;
+      other_player.other_team = data.team;
       socket.emit('new_turn', data);
-      socket.broadcast.to(socket.room_id).emit('new_turn', rooms[socket.room_id].complete);
+      socket.broadcast.to(socket.room_id).emit('new_turn', other_player);
 
       rooms[socket.room_id].complete = false;
     } else {
